d6b1677a40269f1033a4693a1c063e408645c554
Arunanshu655 <arunanshudey7@gmail.com>
2024-12-12 19:42:27 +0530

    modal updated

--- client/src/Components/Modal.js
+++ client/src/Components/Modal.js
@@ -1,5 +1,32 @@
-const Modal = () => {
-    return <></>;
+import { useEffect } from "react";
+import "./Modal.css"
+const Modal = ({ setModalOpen, contract }) => {
+    const sharing = async () => {
+        const address = document.querySelector(".address").value;
+        await contract.allow(address);
+        console.log("shared")
+        setModalOpen(false);
+    };
+
+    return <>
+        <div className="modalBAckground">
+            <div className="modalContainer">
+                <div className="title">Share with</div>
+                <div className="body">
+                    <input type="text" className="address" />
+                </div>
+                <form action="" id="myform">
+                    <select name="" id="" className="selectNumber">
+                        <option value="" className="address">People with Access</option>
+                    </select>
+                </form>
+                <div className="footer">
+                    <button id="cancelBtn" onClick={() => { setModalOpen(false) }}>Cancel</button>
+                    <button id="cancelBtn" onClick={() => sharing()}>Share</button>
+                </div>
+            </div>
+        </div>
+    </>;
 };
 
 export default Modal;
